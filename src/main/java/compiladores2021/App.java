77d1728cb4e173dcbf5eb2691f86f9736ebc6d11
Maximiliano Eschoyez <maximiliano.eschoyez@unc.edu.ar>
2021-08-10 16:54:40 -0300

    inicializacion

---
+++ src/main/java/compiladores2021/App.java
@@ -0,0 +1,13 @@
+package compiladores2021;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
